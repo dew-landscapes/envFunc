a6f37899b0ae04beef7c3612861bcf09547258ad
Acanthiza <nigel.willoughby@sa.gov.au>
2024-09-26 06:59:57 +0930

    Overhaul make_aoi inc. breaking change: level -> filt_level

--- R/make_aoi.R
+++ R/make_aoi.R
@@ -2,14 +2,14 @@
 
 #' Make a region (area) of interest
 #'
-#' @param layer sf
-#' @param filt_col Character name of column in `layer` to filter on (or `NULL`
+#' @param polygons sf
+#' @param filt_col Character name of column in `polygons` to filter on (or `NULL`
 #' for no filter)
-#' @param level Level(s) of `filt_col` to filter
+#' @param filt_level Level(s) of `filt_col` to filter
 #' @param buffer Numeric (or `FALSE`). Distance to buffer in units of `buf_crs`
 #' @param bbox Logical. Return a bounding box around the result?
 #' @param clip sf. If not null, this will be used to clip back the original
-#' `layer` +/- filtered +/- buffered +/- bbox. CHECK both CRS and `clip_buf`
+#' `polygons` +/- filtered +/- buffered +/- bbox. CHECK both CRS and `clip_buf`
 #' @param clip_buf Numeric (or `FALSE`). Distance to buffer `clip` in units of `clip`
 #' @param buf_crs \href{https://epsg.io/}{EPSG} code to use when buffering
 #' @param out_crs \href{https://epsg.io/}{EPSG} code for coordinate system of
@@ -19,9 +19,9 @@
 #' @export
 #'
 #' @examples
-  make_aoi <- function(layer
+  make_aoi <- function(polygons
                        , filt_col = NULL
-                       , level = NULL
+                       , filt_level = NULL
                        , simplify = TRUE
                        , buffer = FALSE
                        , bbox = FALSE
@@ -33,24 +33,41 @@
 
     if(!is.null(clip)) {
 
-      clip <- clip %>%
+      if(clip_buf > 0) clip <- clip %>%
         sf::st_transform(crs = buf_crs) %>%
-        # always buffer slightly to get away from coastline
-        sf::st_buffer(clip_buf) %>%
-        # to out_crs
+        sf::st_buffer(clip_buf)
+
+      clip <- clip %>%
         sf::st_transform(crs = out_crs) %>%
         sf::st_make_valid()
 
     }
 
-    aoi <- layer %>%
-      {if(!is.null(filt_col)) (.) %>% dplyr::filter(grepl(paste0(level, collapse = "|"), !!rlang::ensym(filt_col))) else (.)} %>%
-      {if(simplify) (.) %>% dplyr::summarise() else(.)} %>%
-      {if(buffer) (.) %>% sf::st_transform(crs = buf_crs) %>% sf::st_buffer(buffer) else (.)} %>%
-      # to out_crs
-      sf::st_transform(crs = out_crs) %>%
-      {if(bbox) (.) %>% sf::st_bbox() %>% sf::st_as_sfc() else (.)} %>%
-      {if(!is.null(clip)) (.) %>% sf::st_intersection(clip) else (.)} %>%
+    if(!is.null(filt_col)) aoi <- polygons %>%
+        dplyr::filter(grepl(paste0(filt_level, collapse = "|"), level)) %>%
+        sf::st_make_valid()
+
+    if(simplify) aoi <- aoi %>%
+        dplyr::summarise() %>%
+        sf::st_make_valid()
+
+    if(buffer > 0) aoi <- aoi %>%
+        sf::st_transform(crs = buf_crs) %>%
+        sf::st_buffer(buffer)
+
+    aoi <- aoi %>%
+      sf::st_transform(crs = out_crs)
+
+    if(!is.null(clip)) aoi <- aoi %>%
+        sf::st_intersection(clip)
+
+    # bbox last to esnure
+    if(bbox) aoi <- aoi %>%
+        sf::st_bbox() %>%
+        sf::st_as_sfc() %>%
+        sf::st_sf()
+
+    aoi <- aoi %>%
       sf::st_make_valid()
 
     return(aoi)

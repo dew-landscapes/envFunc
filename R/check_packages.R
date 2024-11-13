f253368e0e03be480706882a99f803e401a2bb59
Acanthiza <nigel.willoughby@sa.gov.au>
2024-11-13 16:23:22 +1030

    check_packages

---
+++ R/check_packages.R
@@ -0,0 +1,39 @@
+#' Check packages
+#'
+#' Check that packages are installed and install them if not. Optionally, make
+#' sure that the most recent version of 'env' packages is installed.
+#'
+#' @param packages Character. Packages to check
+#' @param update_env Logical. Check for any updates to 'env' packages?
+#'
+#' @return Unique, sorted vector of package names
+#' @export
+#'
+#' @examples setup_packages(c("dplyr", "envFunc"))
+check_packages <- function(packages, update_env = FALSE) {
+
+  packages <- sort(unique(packages))
+
+  # check for missing
+  new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+
+  new_packages <- new_packages[!grepl("*env", new_packages)]
+
+  if(length(new_packages)) install.packages(new_packages)
+
+  if(update_env) {
+
+    # update 'env' packages
+    env_packages <- packages[grepl("*env", packages)]
+
+    purrr::walk(env_packages
+                , \(x) remotes::install_github(paste0("acanthiza/", x)
+                                               , dependencies = FALSE
+                                               )
+                )
+
+  }
+
+  return(packages)
+
+}

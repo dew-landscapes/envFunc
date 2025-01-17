d61b56f7a5ed6abb28d6389eb408eea9ed41730a
Acanthiza <nigel.willoughby@sa.gov.au>
2025-01-17 14:42:08 +1030

    more arguments to make_targets

--- R/make_targets.R
+++ R/make_targets.R
@@ -1,29 +1,37 @@
 
 #' Make targets
 #'
-#' Runs `targets::tar_source()`, reads the context from `settings_context_file`,
-#' and creates _targets.yaml from any files matching the pattern
-#' `"^\\d{3}_.*\\.R$"`.
+#' Runs `targets::tar_source()`, creates and then reads _targets.yaml from any
+#' files matching the pattern ``.
 #'
-#' @param settings_context_file
+#' @param settings Character path to yaml settings file or named, nested list.
+#' Passed to `set_list` argument of `envFunc::name_env_out()`. If character, the
+#' named, nested list will be `yaml::read_yaml(settings)$context`
+#' @param base_dir Character. Path at which to create the store.
+#' @param target_pattern Character. Regexp to find targets ['project files']
+#' (https://books.ropensci.org/targets/projects.html#project-files).
+#'
 #'
 #' @return list of 'projects' each with elements 'script' and 'store'. Saves
 #' _targets.yaml
 #' @export
 #'
 #' @examples
-make_targets <- function(settings_context_file = "settings/setup.yaml") {
+make_targets <- function(settings = "settings/setup.yaml"
+                         , base_dir = fs::path("..", "..", "out")
+                         , target_pattern = "^\\d{3}_.*\\.R$"
+                         ) {
 
-  targets::tar_source()
-
-  settings_context <- yaml::read_yaml(settings_context_file)$context
+  set_list <- if(is.character(settings)) yaml::read_yaml(settings)$context else settings
 
   # tars --------
   ## tars df ------
-  tars_df <- tibble::tibble(script = fs::dir_ls(regexp = "^\\d{3}_.*\\.R$")) |>
+  tars_df <- tibble::tibble(script = fs::dir_ls(regexp = target_pattern)) |>
     dplyr::mutate(project = purrr::map_chr(script, \(x) gsub("\\d{3}_|\\.R", "", x))
                   , order = readr::parse_number(script)
-                  , store = envFunc::store_dir(set_list = c(settings_context))
+                  , store = envFunc::store_dir(set_list = set_list
+                                               , base_dir = base_dir
+                                               )
                   , store = fs::path(store, project)
                   ) |>
     dplyr::arrange(order) |>

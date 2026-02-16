6290c87aae1f0496e5c3ecc9d2481bf9c9f60291
Acanthiza <nigel.willoughby@sa.gov.au>
2026-02-16 12:40:06 +1030

    Change the default parameters to extract_scale

--- R/extract_scale.R
+++ R/extract_scale.R
@@ -4,9 +4,10 @@
 #' change from 'default'. `NULL` values in default are maintained. Set a
 #' non-default list element to `NULL` to remove it from default.
 #'
-#' @param element Name of element in `scales` for which to extract the
-#' scale.
+#' @param element Name (or index) of element in `scales` for which to extract
+#' the scale.
 #' @param scales Path to yaml containing the scales relevant to a project.
+#' @param element_default Name (or index) of default element in `scales`.
 #'
 #' @returns Named, nested list describing `element`
 #' @export
@@ -20,18 +21,28 @@
 #' extract_scale(scales = settings)
 #' extract_scale(element = "change", scales = settings)
 #'
-extract_scale <- function(element = "default"
+extract_scale <- function(element = 1
                           , scales = "settings/scales.yaml"
+                          , element_default = 1
                           ) {
 
   if(is.character(scales)) scales <- yaml::read_yaml(scales)
 
-  scale <- scales[["default"]]
+  # check that element is a name within scales
+  stopifnot(element %in% names(scales))
 
-  if(element != "default") {
+  # check that scales is now a list
+  stopifnot(is.list(scales))
+
+  use_element <- if(is.character(element)) which(names(scales) == element) else element
+  use_default <- if(is.character(element_default)) which(names(scales) == element_default) else element_default
+
+  scale <- scales[[use_default]]
+
+  if(isTRUE(use_element != use_default)) {
 
     scale <- modifyList(scale
-                        , scales[[element]]
+                        , scales[[use_element]]
                         )
 
   }
